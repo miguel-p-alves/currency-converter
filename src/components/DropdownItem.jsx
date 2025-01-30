a2a4ac945416ef1ad71ea5517321079db629a52d
Miguel Alves <miguelpalves01@gmail.com>
2025-01-30 09:59:38 -0300

    dropdown text-color fixed on dark theme

--- src/components/DropdownItem.jsx
+++ src/components/DropdownItem.jsx
@@ -2,7 +2,7 @@
 const DropdownItem = ({ children, onClick }) => {
   return (
     <div
-      className="p-[0.5rem] m-[0.1rem] w-[100%] rounded-[0.5rem] cursor-pointer hover:text-blue-600 dropdown-item]"
+      className="p-[0.5rem] m-[0.1rem] w-[100%] rounded-[0.5rem] cursor-pointer hover:text-blue-600 dark:text-black dropdown-item]"
       onClick={onClick}
     >
       {children}
